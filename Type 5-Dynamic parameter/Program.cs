69ba68bfd0df39540e4f31207ffcfc32afe04a81
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:33:19 +0530

    Add project files.

---
+++ Type 5-Dynamic parameter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Type_5_Dynamic_parameter
+{
+    class Program
+    {
+       static void show(dynamic num)
+        {
+            Console.WriteLine("All "+num);
+        }
+        static void Main(string[] args)
+        {
+            show(12);
+            show("alshay");
+            show(12.4);
+            show(true);
+
+
+            
+        }
+    }
+}
